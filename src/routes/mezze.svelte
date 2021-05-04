df81d481a3b0112ba8a70b57e68c6fef61a96348
Sigmundur Mørkøre <sm@sigmundur.dev>
2021-05-04 06:02:12 +0000

    ✨ Add mezze recipie

---
+++ src/routes/mezze.svelte
@@ -0,0 +1,136 @@
+<script>
+	import Jitsi from '../lib/Jitsi.svelte';
+</script>
+
+<article>
+	<header>
+		<h1>Mezze Maniche Alla Gricia</h1>
+	</header>
+	<main class="instructions">
+		<section>
+			<h2>Ingredients</h2>
+			<table>
+				<tr>
+					<th>Amount</th>
+					<th>Ingredient</th>
+				</tr>
+				<tr>
+					<td>400g</td>
+					<td>Dried Mezze Maniche or Rigatoni</td>
+				</tr>
+				<tr>
+					<td>1 1/2 cups (200g)</td>
+					<td>Guanciale or Pancetta</td>
+				</tr>
+				<tr>
+					<td>2 cups + ½ cup (200g + 50g)</td>
+					<td>Grated Pecorino Romano</td>
+				</tr>
+				<tr>
+					<td>1 tsp (5g)</td>
+					<td>Fresh Cracked Black Pepper</td>
+				</tr>
+			</table>
+		</section>
+
+		<section>
+			<h2>Directions</h2>
+			<ol>
+				<li>Bring a large pot of water to a boil, and salt generously.</li>
+				<li>
+					In a large frying pan render out the guanciale, with a little bit of vegetable oil, on low
+					heat until the guanciale is crispy. Add in the black pepper and briefly toast. Add in a
+					splash of the pasta water to stop the cooking process, and keep aside.
+				</li>
+				<li>
+					Cook the pasta until al dente, or roughly 3 minutes less than the package instructions.
+				</li>
+				<li>
+					Once the pasta is cooked add it to the pan, reserving 1 cup of the pasta water, over low
+					heat and toss to coat the pasta in the guanciale.
+				</li>
+				<li>
+					While tossing the pasta, sprinkle in 200g of the Pecorino Romano, to bring the sauce
+					together,and coat the pasta. If there is too much liquid, add more cheese to thicken, if
+					the cheese is clumping or too thick, add some of the reserved pasta water.
+				</li>
+				<li>Plate the pasta, and garnish with the remaining 50g Pecorino Romano.</li>
+			</ol>
+		</section>
+	</main>
+
+	<Jitsi roomName="CoCooking-pakistani-vegetable" />
+</article>
+
+<style>
+	main {
+		padding: 10px;
+	}
+
+	table {
+		width: 100%;
+	}
+
+	table,
+	th,
+	td {
+		border: 1px solid black;
+	}
+
+	section {
+		padding-top: 1rem;
+	}
+	section h2 {
+		font-family: Montserrat;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 24px;
+		line-height: 29px;
+		display: flex;
+		align-items: center;
+	}
+	ol {
+		padding-left: 30px;
+		list-style: decimal;
+	}
+	li {
+		font-size: 18px;
+		line-height: 163.9%;
+	}
+
+	article {
+		width: 100%;
+		display: grid;
+		grid-gap: 1rem;
+		grid-template-areas:
+			'header header'
+			'instructions jitsi ';
+	}
+
+	header {
+		grid-area: header;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		height: 350px;
+		width: 100%;
+
+		background: linear-gradient(0deg, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),
+			url('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-4NrMVrgk5kY%2FU1EqBsSPI8I%2FAAAAAAAABmY%2FMdLuI5j-5xQ%2Fs1600%2FDSC_0653.JPG&f=1&nofb=1');
+		box-shadow: inset 0px -130px 100px #ffffff;
+
+		background-size: cover;
+		background-position: center;
+	}
+	header h1 {
+		font-family: Montserrat;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 48px;
+		line-height: 59px;
+		display: flex;
+		align-items: center;
+		text-align: center;
+	}
+</style>
