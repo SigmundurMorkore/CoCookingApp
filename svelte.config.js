268cb4f00e33133203761fbf6ed719af3a3247d7
Sigmundur Mørkøre <sm@sigmundur.dev>
2021-05-05 12:49:46 +0000

    🔧 Convert svelte.config.cjs to a .js file

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
+
+export default {
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+		target: '#svelte'
+	}
+};
